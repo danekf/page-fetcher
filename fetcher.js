98b518aa6f75873fa6345ed3fe5042ab6f2e8b3e
Danek <danek.fill@outlook.com>
2022-07-07 17:40:54 +0000

    saving works now with CL arguments

--- fetcher.js
+++ fetcher.js
@@ -1,25 +1,27 @@
+'use strict';
+
 const request = require('request');
 const fs = require('fs');
 
-const fetchSite = (URL) => {
-  request(URL, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the page.
+const fetchSite = (url, directory) => {
+  request(url, (error, response, body) => {
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the page.
 
-  fs.writeFile('./test.html', body, err => {
+  fs.writeFile( directory, body, err => {
     if (err) {
       console.error(err);
     }
-    // file written successfully
+    console.log(`${url} saved to ${directory}.`)
   });
-
-
-  
-
 });}
 
-fetchSite('http://www.google.com');
+let url = process.argv[2];
+console.log(url);
+let directory = process.argv[3];
+console.log(directory);
+fetchSite(url, directory);
 
 
 
